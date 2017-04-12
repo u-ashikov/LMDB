f7b4a651bc9cff33a4c6afc01093ef357c7d69ab
u-ashikov <u.ashikov@abv.bg>
2017-04-12 17:10:10 +0300

    ViewModels project changes

--- LMDB.Web/Controllers/ManageController.cs
+++ LMDB.Web/Controllers/ManageController.cs
@@ -1,13 +1,13 @@
-﻿using System.Linq;
-using System.Threading.Tasks;
-namespace LMDB.Web.Controllers
+﻿namespace LMDB.Web.Controllers
 {
     using System.Web;
     using System.Web.Mvc;
     using Microsoft.AspNet.Identity;
     using Microsoft.AspNet.Identity.Owin;
     using Microsoft.Owin.Security;
-    using LMDB.Web.ViewModels.Manage;
+    using LMDB.ViewModels.Manage;
+    using System.Linq;
+    using System.Threading.Tasks;
 
     [Authorize]
     public class ManageController : Controller
