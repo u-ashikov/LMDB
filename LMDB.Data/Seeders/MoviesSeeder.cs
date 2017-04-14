7df6519fdcacfc3ec8cdf0906ed85e69c8d5f96b
u-ashikov <u.ashikov@abv.bg>
2017-04-14 15:42:48 +0300

    Changes

--- LMDB.Data/Seeders/MoviesSeeder.cs
+++ LMDB.Data/Seeders/MoviesSeeder.cs
@@ -15,8 +15,10 @@
             var movies =
                 File.ReadAllText(docPath).Split(new[] {'\n', '\r'}, StringSplitOptions.RemoveEmptyEntries).ToArray();
 
-            var contributors = context.Contributors;
-            var contributorsCount = contributors.Count();
+            var actors = context.Actors;
+            var actorsCount = actors.Count();
+            var directors = context.Directors;
+            var directorsCount = directors.Count();
             var rand = new Random();
 
             for (int i = 1; i < movies.Length; i++)
@@ -25,9 +27,8 @@
                 var indexOfYear = line[0].IndexOf('(');
                 var title = line[0].Substring(0, indexOfYear - 1);
                 var year = int.Parse(line[0].Substring(indexOfYear + 1, 4));
-                var director = contributors.Find(i%contributorsCount + 1);
-
-                if (director == null || (director is Actor)) continue;
+                var director = directors.Find(i%directorsCount + 1);
+                if (director == null) continue;
 
                 var movie = new Movie
                 {
@@ -45,8 +46,7 @@
 
                 for (int j = 0; j < 6; j++)
                 {
-                    //var actor = contributors.Find(j * i % contributorsCount + 1);
-                    //movie.a.Add(actor);
+                    movie.Actors.Add(actors.Find(j * i % actorsCount + 1));
                 }
 
                 context.Movies.AddOrUpdate(m => m.Title, movie);
