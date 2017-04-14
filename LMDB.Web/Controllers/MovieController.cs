fc15b455f5fbb1c641e5fb9daca2460ccc130a7a
Georgi Georgiev <gogo4ds@gmail.com>
2017-04-15 00:20:05 +0300

    added Movies View and Controller folders

---
+++ LMDB.Web/Controllers/MovieController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace LMDB.Web.Controllers
+{
+    public class MovieController : Controller
+    {
+        public ActionResult AllMovies()
+        {
+            ViewBag.Message = "All movies in one place";
+
+            return View();
+        }
+    }
+}
