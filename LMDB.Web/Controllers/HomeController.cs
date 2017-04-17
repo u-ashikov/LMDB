29fa02471ef44c5286cbe9ac55c7e345429f377f
u-ashikov <u.ashikov@abv.bg>
2017-04-17 18:20:24 +0300

    User roles

--- LMDB.Web/Controllers/HomeController.cs
+++ LMDB.Web/Controllers/HomeController.cs
@@ -13,6 +13,7 @@ namespace LMDB.Web.Controllers
             return View();
         }
 
+        [Authorize(Roles = "Admin")]
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
--- LMDB.Web/Startup.cs
+++ LMDB.Web/Startup.cs
@@ -1,5 +1,11 @@
-﻿using Microsoft.Owin;
+﻿using System;
+using Microsoft.Owin;
 using Owin;
+using LMDB.Data;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using LMDB.Models;
+using System.Linq;
 
 [assembly: OwinStartupAttribute(typeof(LMDB.Web.Startup))]
 namespace LMDB.Web
@@ -9,6 +15,29 @@ namespace LMDB.Web
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            createRolesAndUser();
+        }
+
+        private void createRolesAndUser()
+        {
+            MoviesContext context = new MoviesContext();
+
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+
+            if (!roleManager.Roles.Any())
+            {
+                var roleCreated = roleManager.Create(new IdentityRole("Admin"));
+                if (roleCreated.Succeeded)
+                {
+                    var user = userManager.Users.FirstOrDefault(u => u.UserName == "gergan");
+                    if (user!=null)
+                    {
+                        userManager.AddToRole(user.Id, "Admin");
+                    } 
+                }
+            }
+            
         }
     }
 }
