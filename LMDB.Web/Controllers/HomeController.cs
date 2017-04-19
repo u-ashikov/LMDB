34f5670a58a515d0488ed4ffc5ecca9c06e9e16c
Georgi Georgiev <gogo4ds@gmail.com>
2017-04-19 16:13:19 +0300

    Carousel check if posters exist

--- LMDB.Web/Controllers/HomeController.cs
+++ LMDB.Web/Controllers/HomeController.cs
@@ -1,31 +1,30 @@
 ﻿namespace LMDB.Web.Controllers
 {
     using Data;
-    using Models;
     using System;
     using System.Collections.Generic;
     using System.Data.Entity;
-    using System.Linq;
-    using System.Web;
     using System.Web.Mvc;
 
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
-            var random = new Random();
             var context = new MoviesContext();
             var randomPosters = new List<string>();
 
             context.Movies.Load();
             var movies = context.Movies.Local;
-
-            for (int i = 0; i < 8; i++)
+            try
             {
-                //var index = random.Next(0, movies.Count - 1);
-                var poster = "data:image/jpeg;base64," + Convert.ToBase64String(movies[i].Poster);
-                randomPosters.Add(poster);
+                for (int i = 0; i < 8; i++)
+                {
+                    //var index = random.Next(0, movies.Count - 1);
+                    var poster = "data:image/jpeg;base64," + Convert.ToBase64String(movies[i].Poster);
+                    randomPosters.Add(poster);
+                }
             }
+            catch (Exception) { ViewBag.Posters = randomPosters; }
 
             ViewBag.Posters = randomPosters;
 
