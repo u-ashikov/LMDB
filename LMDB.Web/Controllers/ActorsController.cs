7938c15592a8224543cbd1f041bf4e0642d16993
Georgi Georgiev <gogo4ds@gmail.com>
2017-04-19 18:41:42 +0300

    Actor and Director Controllers added

---
+++ LMDB.Web/Controllers/ActorsController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using LMDB.Data;
+using LMDB.Models;
+
+namespace LMDB.Web.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private MoviesContext db = new MoviesContext();
+
+        // GET: Actors
+        public ActionResult Index()
+        {
+            var actors = db.Actors.Include(a => a.Country);
+            return View(actors.ToList());
+        }
+
+        // GET: Actors/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Actor actor = db.Actors.Find(id);
+            if (actor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(actor);
+        }
+
+        // GET: Actors/Create
+        public ActionResult Create()
+        {
+            ViewBag.CountryId = new SelectList(db.Countries, "Id", "Name");
+            return View();
+        }
+
+        // POST: Actors/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,FirstName,LastName,Biography,Birthdate,Picture,CountryId")] Actor actor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Actors.Add(actor);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CountryId = new SelectList(db.Countries, "Id", "Name", actor.CountryId);
+            return View(actor);
+        }
+
+        // GET: Actors/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Actor actor = db.Actors.Find(id);
+            if (actor == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CountryId = new SelectList(db.Countries, "Id", "Name", actor.CountryId);
+            return View(actor);
+        }
+
+        // POST: Actors/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,FirstName,LastName,Biography,Birthdate,Picture,CountryId")] Actor actor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(actor).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CountryId = new SelectList(db.Countries, "Id", "Name", actor.CountryId);
+            return View(actor);
+        }
+
+        // GET: Actors/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Actor actor = db.Actors.Find(id);
+            if (actor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(actor);
+        }
+
+        // POST: Actors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Actor actor = db.Actors.Find(id);
+            db.Actors.Remove(actor);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ LMDB.Web/Controllers/DirectorsController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using LMDB.Data;
+using LMDB.Models;
+
+namespace LMDB.Web.Controllers
+{
+    public class DirectorsController : Controller
+    {
+        private MoviesContext db = new MoviesContext();
+
+        // GET: Directors
+        public ActionResult Index()
+        {
+            var directors = db.Directors.Include(d => d.Country);
+            return View(directors.ToList());
+        }
+
+        // GET: Directors/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Director director = db.Directors.Find(id);
+            if (director == null)
+            {
+                return HttpNotFound();
+            }
+            return View(director);
+        }
+
+        // GET: Directors/Create
+        public ActionResult Create()
+        {
+            ViewBag.CountryId = new SelectList(db.Countries, "Id", "Name");
+            return View();
+        }
+
+        // POST: Directors/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,FirstName,LastName,Biography,Birthdate,Picture,CountryId")] Director director)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Directors.Add(director);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CountryId = new SelectList(db.Countries, "Id", "Name", director.CountryId);
+            return View(director);
+        }
+
+        // GET: Directors/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Director director = db.Directors.Find(id);
+            if (director == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CountryId = new SelectList(db.Countries, "Id", "Name", director.CountryId);
+            return View(director);
+        }
+
+        // POST: Directors/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,FirstName,LastName,Biography,Birthdate,Picture,CountryId")] Director director)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(director).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CountryId = new SelectList(db.Countries, "Id", "Name", director.CountryId);
+            return View(director);
+        }
+
+        // GET: Directors/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Director director = db.Directors.Find(id);
+            if (director == null)
+            {
+                return HttpNotFound();
+            }
+            return View(director);
+        }
+
+        // POST: Directors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Director director = db.Directors.Find(id);
+            db.Directors.Remove(director);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
